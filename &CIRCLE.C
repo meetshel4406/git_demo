0f3b513957b8cba564dc623fe14d9f3d5b083e90
meet <msheladiya7@gmail.com>
2022-06-13 17:27:27 +0530

    firstcommit13062022

---
+++ &CIRCLE.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	  float p,r,l,w,h,b,z;
+	  clrscr();
+	  printf("Enter value of p :");
+	  scanf("%f",&p);
+	  printf("Enter value of r :");
+	  scanf("%f",&r);
+
+	  z=p*r*r;
+	  printf("\n calculate area of circle = p*r*r : %f",z);
+	  getch();
+
+
+}
